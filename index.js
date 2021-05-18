53f7e12e5cc02e03997c3ce2877cc5d492604991
Mike Robards <mikerobards@icloud.com>
2021-05-18 16:51:14 -0400

    wrote getPhotosHTML function

--- index.js
+++ index.js
@@ -13,6 +13,21 @@
             Each photo should be at least 1rem away from the photos around it
 */
 
+/* 
+    2. 
+       Write a function to create the my-photos Component
+       
+       Create a Column Flexbox container around my-photos
+       
+       Create a new img and place it, centered, above my-photos
+        -This should be hidden initially
+        -Also give it the my-photo class
+    
+       Create a "click" event for each photo in my-photos
+        -When clicked, load the clicked photo into the new img tag
+*/
+
+
 
 async function getPhotos() {
     let res = await fetch("photos.json")
@@ -20,14 +35,16 @@ async function getPhotos() {
     return photos
 }
 
-getPhotos().then(photos => {
+function getPhotosHTML(photos) {
     let myPhotosHTML = photos.map(photo => {
         return `<img src="https://picsum.photos/id/${photo.id}/100/100" alt="${photos.title}" class="my-photo">`
     }).join('')
 
     console.log(myPhotosHTML)
 
-    document.body.innerHTML = `
-        <div class="my-photos">${myPhotosHTML}</div>
-    `
+    return `<div class="my-photos">${myPhotosHTML}</div>`
+}
+
+getPhotos().then(photos => {
+    document.body.innerHTML = getPhotosHTML(photos)
 })
