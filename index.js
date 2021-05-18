9c0efa061310fad626f442efdacff40fd1b7f1b0
Mike Robards <mikerobards@icloud.com>
2021-05-18 17:05:12 -0400

    finished photo gallery 2

--- index.js
+++ index.js
@@ -46,5 +46,18 @@ function getPhotosHTML(photos) {
 }
 
 getPhotos().then(photos => {
-    document.body.innerHTML = getPhotosHTML(photos)
+    document.body.innerHTML = `<div class="my-gallery">
+    <img style="display: none;" class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/1/200/200">
+    ${getPhotosHTML(photos)}</div>`
+
+    let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"))
+    myPhotoImgs.forEach(photoImg => {
+        photoImg.addEventListener("click", event=> {
+            let selectedPhotoSrc = `${photoImg.src.substr(0, photoImg.src.length - 7)}200/200`
+            let selectedPhoto = document.getElementById("my-selected-photo")
+            selectedPhoto.src = selectedPhotoSrc
+            selectedPhoto.style.display = "inline"
+            
+        })
+    })
 })
